3fb48fb21911b39a3de4fca2975eea88b212a6c0
russellgoldenberg <russellgoldenberg@gmail.com>
2022-03-04 15:08:02 -0500

    update github pages

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,9 +1,7 @@
 <script>
-  import Demo from "$components/helpers/Demo.svelte";
   import WIP from "$components/helpers/WIP.svelte";
-  // import Footer from "$components/Footer.svelte";
 </script>
 
 <WIP />
-<Demo />
-<!-- <Footer /> -->
+
+<h1>Cough</h1>
