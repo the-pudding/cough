ab340d6d6d637b441b93ac1482abee6ff574ee5c
russellgoldenberg <russellgoldenberg@gmail.com>
2022-03-08 10:26:48 -0500

    play clip

--- src/components/Video.svelte
+++ src/components/Video.svelte
@@ -3,11 +3,34 @@
   export let film;
   export let fig;
   export let caption;
+
+  let w;
+  let yt;
 </script>
 
 <figure>
-  <img src="https://i.ytimg.com/vi/{id}/maxresdefault.jpg" alt="coughs in the film {film}" />
-  <figcaption>Fig. {fig} {caption}</figcaption>
+  <div class="inner">
+    <div class="image" bind:clientWidth={w}>
+      <img src="https://i.ytimg.com/vi/{id}/maxresdefault.jpg" alt="coughs in the film {film}" />
+    </div>
+    {#if yt}
+      <div class="yt">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/{id}?controls=0&autoplay=1"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </div>
+    {/if}
+  </div>
+  <figcaption>
+    Fig. {fig}
+    {caption} <button on:click={() => (yt = true)}>Play Clip</button>
+  </figcaption>
 </figure>
 
 <style>
@@ -17,9 +40,30 @@
     margin-bottom: 2rem;
   }
 
+  .inner {
+    position: relative;
+  }
+
+  .yt {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
   figcaption {
     margin-top: 0.5rem;
     line-height: 1.2;
     font-size: 0.9em;
+  }
+
+  button {
+    padding: 0;
+    background: transparent;
+    color: var(--color-link);
+    text-decoration: underline;
+    font-family: inherit;
+    border-radius: 0;
   }
 </style>
